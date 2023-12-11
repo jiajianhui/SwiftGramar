736ef8087b8e26cf00eb82715c9a83dbd5d780a4
jianhui <jia15176168273@icloud.com>
2023-12-11 15:33:33 +0800

    Initial Commit

---
+++ SwiftGramar/SwiftGramarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGramarApp.swift
+//  SwiftGramar
+//
+//  Created by 贾建辉 on 2023/12/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGramarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
